ad895b81cb945f9fa5fcfb0b0162521d3d281b71
Maukie <maurickberends@hotmail.com>
2026-01-10 17:32:35 +0100

    initial commit

---
+++ main.lua
@@ -0,0 +1,12 @@
+print("Lua is working!")
+print("Hello, World!")
+
+local function add(a, b)
+    return a + b
+end
+
+local sum = add(5, 7)
+print("The sum of 5 and 7 is: " .. sum)
+
+local hello = "Hello, Lua!"
+print(hello)
