876241ee85b94fa9cd63f792e65298be027f2094
Maukie <maurickberends@hotmail.com>
2026-01-11 18:42:07 +0100

    Initial Setup

--- main.lua
+++ main.lua
@@ -1,18 +1,1 @@
 print("Lua is working!")
-print("Hello, World!")
-
-local function add(a, b)
-    return a + b
-end
-
-local sum = add(5, 7)
-print("The sum of 5 and 7 is: " .. sum)
-
-local hello = "Hello, Lua!"
-print(hello)
-
-for i = 1, 5 do
-    print("Count: " .. i)
-end
-
-print("Script finished.")
