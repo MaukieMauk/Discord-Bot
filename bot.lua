f490167c6843bb283dd45fbf24eb797d247d0735
Maukie <maurickberends@hotmail.com>
2026-01-11 19:19:18 +0100

    secundaire setup

--- bot.lua
+++ bot.lua
@@ -1,20 +1,19 @@
 local discordia = require('discordia')
 local client = discordia.Client()
 
-client:on('ready', function()
-    print('Bot logged in as ' .. client.user.username)
-end)
-
-client:on('messageCreate', function(message)
-    if message.content == '!ping' then
-        message:reply('Pong!')
+local function readToken(filename)
+    local file = io.open(filename, "r")
+    if not file then
+        error("Token file not found: " .. filename)
     end
-end)
-
--- Use the environment variable BOTKEY instead of hardcoding the token
-local token = os.getenv("BOTKEY")
-if not token then
-    error("BOTKEY environment variable not set!")
+    local token = file:read("*l")  -- read the first line
+    file:close()
+    return token
 end
 
-client:run(token)
+local token = readToken("config.txt")  -- path to your file
+if not token then
+    error("Token is empty!")
+end
+
+client:run("Bot " .. token)
